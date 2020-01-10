569a99746b035f28dc480a97db6956269c47d826
lgope <ilakshmangope@gmail.com>
2020-01-10 21:35:04 +0600

    added new feature 'Rank Badge' 🗽📈

--- Front-End/src/Components/Rank/Rank.component.jsx
+++ Front-End/src/Components/Rank/Rank.component.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+const emojis = ['😄', '😃', '😀', '😊', '😉', '😍', '🔶', '🔷', '🚀'];
+
+class Rank extends Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = rank => {
+    const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+    this.setState({ emoji: rankEmoji });
+  };
+  render() {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>{entries}</div>
+        <div className='white f3'>{`Rank Badge: ${this.state.emoji}`}</div>
       </div>
-      <div className='white f1'>{entries}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Rank;
